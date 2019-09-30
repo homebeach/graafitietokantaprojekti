71ab8a4c173dfa053087b7967de46aeb45dc6f5e
SSSvensk <sss2341@hotmail.com>
2019-09-30 09:51:55 +0300

    added recursive predicates

--- src/main/java/XMLToCypher/AppCopy.java
+++ src/main/java/XMLToCypher/AppCopy.java
@@ -1,27 +1,35 @@
-package XMLToCypher;
+package com.cypherTest.cypherTest;
+
+import java.util.*;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.TransactionWork;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.neo4j.driver.v1.*;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
 
-import java.util.List;
-
+import static org.neo4j.driver.v1.Values.parameters;
 /**
  * Hello world!
  *
  */
-public class AppCopy
+public class App 
 {
     @SuppressWarnings("deprecation")
-	public static void main( String[] args ) {
-    	try {
-
-    		//syötteitä * a/b/c a[@b]
-            // olettaa yhden syötteen args 0:sta
-    		String inputCmd = null;
+    public static void main( String[] args ) {
+        try {
+            String inputCmd = null;
             if ( args.length>0 ) inputCmd = args[0];
             // create a CharStream that reads from standard input
             ANTLRInputStream input = new ANTLRInputStream(inputCmd);
@@ -43,25 +51,25 @@ public class AppCopy
 
             String cypQ = mylistener.wholeQuery.toString();
             System.out.println(cypQ);
+            /*
+            Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "testi" ) );
+            Session session = driver.session();
+            StatementResult stre = session.run(cypQ);
+            List<Record> lr = stre.list();
+            System.out.println(lr.size());
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            for (int i = 0; i < lr.size(); i++) {
+                String json = gson.toJson(gson.toJson(lr.get(i).asMap()));
+                System.out.println(json);
+            }
+              System.out.println(cypQ);
+            System.out.println(lr.size());
             
-    		Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "testi" ) );
-        	Session session = driver.session();
-        	StatementResult stre = session.run(cypQ);
-        	List<Record> lr = stre.list();
-        	System.out.println(lr.size());
-        	Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        	for (int i = 0; i < lr.size(); i++) {
-        		String json = gson.toJson(gson.toJson(lr.get(i).asMap()));
-        		System.out.println(json);
-        	}
-        	
-        	System.out.println(cypQ);
-        	System.out.println(lr.size());
-        	
-        	driver.close();
-    	} catch (Exception e) {
-    		System.out.println("virhe!");
-    		System.out.println(e);
-    	}
+            driver.close();
+            */
+        } catch (Exception e) {
+            System.out.println("virhe!");
+            System.out.println(e);
+        }
     }
 }
